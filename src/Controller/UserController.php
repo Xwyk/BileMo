bb5a4216c1296f87706a3aa4eb9e29b0c33bc812
florianleboul@gmail.com <florianleboul@gmail.com>
2021-05-27 19:39:24 +0200

    useless use statement removed

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -3,18 +3,13 @@
 namespace App\Controller;
 
 use App\Entity\Client;
-use App\Entity\Product;
 use App\Entity\User;
 use App\Repository\ClientRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use FOS\RestBundle\Controller\AbstractFOSRestController;
 use FOS\RestBundle\Controller\Annotations as Rest;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\HttpKernel\Exception\BadRequestHttpException;
-use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Validator\ConstraintViolationList;
 
 class UserController extends AbstractFOSRestController
