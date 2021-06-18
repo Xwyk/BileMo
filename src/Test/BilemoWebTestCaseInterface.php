e8e880f080d85783f6e31e78fcbc36d72211e27b
Florian LEBOUL <florianleboul@gmail.com>
2021-06-18 19:38:21 +0200

    Visual code update

--- src/Test/BilemoWebTestCaseInterface.php
+++ src/Test/BilemoWebTestCaseInterface.php
@@ -3,11 +3,19 @@
 
 namespace App\Test;
 
-use Symfony\Component\HttpFoundation\Response;
-
 interface BilemoWebTestCaseInterface
 {
     public function loadEntryPoints():void;
     public function testEntryPoints();
-    public function entryPoint(string $name, string $type, string $url, int $expectedCode, array $parameters = [], array $files = [], array $server = [], string $content = "", bool $needReturnOnOK = false);
+    public function entryPoint(
+        string $name,
+        string $type,
+        string $url,
+        int $expectedCode,
+        array $parameters = [],
+        array $files = [],
+        array $server = [],
+        string $content = "",
+        bool $needReturnOnOK = false
+    );
 }
