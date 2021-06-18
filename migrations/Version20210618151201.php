2a35f24dfa57fba45d5abed9b7191e5e53d8e917
Florian LEBOUL <florianleboul@gmail.com>
2021-06-18 17:53:26 +0200

    new migration

---
+++ migrations/Version20210618151201.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20210618151201 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE client CHANGE siret siret VARCHAR(14) NOT NULL, CHANGE siren siren VARCHAR(9) NOT NULL');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE client CHANGE siret siret INT NOT NULL, CHANGE siren siren INT NOT NULL');
+    }
+}
