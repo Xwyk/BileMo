865ce8b38235f2b1ffe3117c135cabb9093c7acd
florianleboul@gmail.com <florianleboul@gmail.com>
2021-06-19 13:43:14 +0200

    Default product added

--- tests/Controller/ProductControllerTest.php
+++ tests/Controller/ProductControllerTest.php
@@ -9,14 +9,24 @@ use Symfony\Component\HttpFoundation\Response;
 
 class ProductControllerTest extends BilemoWebTestCase
 {
+    protected $firstProductId = 1;
+    protected $firstProduct = [
+        "brand"           => "Huawei",
+        "commercial_name" => "b",
+        "model"           => "b",
+        "rom"             => 12,
+        "ram"             => 12,
+        "battery"         => 12,
+        "launched_at"     => "2021-05-27",
+        "price"          => 200
+    ];
     public function loadEntryPoints(): array
     {
-        $productId = 1;
         return [
             "testShowDetailsUnauthenticated" => [
                 [
                     "type" => "GET",
-                    "url" => "/api/products/" . $productId,
+                    "url" => "/api/products/" . $this->firstProductId,
                     "parameters" => [],
                     "files" => [],
                     "server" => [],
@@ -29,14 +39,15 @@ class ProductControllerTest extends BilemoWebTestCase
             "testShowDetailsAuthenticated" => [
                 [
                     "type" => "GET",
-                    "url" => "/api/products/" . $productId,
+                    "url" => "/api/products/" . $this->firstProductId,
                     "parameters" => [],
                     "files" => [],
                     "server" => [],
                     "authenticated" => true,
                     "content" => "",
                     "expectedCode" => Response::HTTP_OK,
-                    "needReturnOnOK" => false
+                    "needReturnOnOK" => true,
+                    "additionalCheck" => "checkShowDetailsAuthenticated"
                 ]
             ],
             "testShowListUnauthenticated" => [
@@ -62,9 +73,41 @@ class ProductControllerTest extends BilemoWebTestCase
                     "authenticated" => true,
                     "content" => "",
                     "expectedCode" => Response::HTTP_OK,
-                    "needReturnOnOK" => false
+                    "needReturnOnOK" => true,
+                    "additionalCheck" => "checkShowListAuthenticated"
                 ]
             ]
         ];
+    }
+
+    /**
+     * Checks first user in list correspond to firs user data defined in class and check _links on each user displayed
+     * @param $result
+     */
+    protected function checkShowListAuthenticated($result){
+
+        $productsList = $result->_embedded->items;
+
+        $this->checkAttributes(
+            (json_decode(json_encode($productsList[0]), true)),
+            (json_decode(json_encode($this->firstProduct), true))
+        );
+
+        foreach ($productsList as $resultProduct){
+            $this->checkLinks($resultProduct, ['self']);
+        }
+
+        $this->checkLinks($result, ['first', 'last', 'self']);
+    }
+
+    /**
+     * Checks if returned user (id defined in class) correspond to data (defined in class) and check _links content
+     * @param $result
+     */
+    protected function checkShowDetailsAuthenticated($result){
+        $this->checkAttributes(
+            (json_decode(json_encode($result), true)),
+            (json_decode(json_encode($this->firstProduct), true))
+        );
     }
 }
